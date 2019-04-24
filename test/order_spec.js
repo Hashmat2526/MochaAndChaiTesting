13aaaae9f50eee01390e3f8db2d9bd712a547518
hashmat2526 <hashmatajalali@gmail.com>
2019-04-24 14:30:27 -0700

    adding sinon spy function

--- test/order_spec.js
+++ test/order_spec.js
@@ -1,18 +1,32 @@
 var expect = require("chai").expect;
 var rewire = require("rewire");
 var order = rewire("../lib/order");
+var sinon = require("sinon");
 
 describe("Ordering Items", function() {
   beforeEach(function() {
     this.testData = [
-      { sku: "AAA", qty: 10 },
+      { sku: "AAB", qty: 10 },
       { sku: "BBB", qty: 0 },
       { sku: "CCC", qty: 3 }
     ];
+
+    // this.console = {
+    //   log: sinon.spy()
+    // };
+    // console.log(this.console);
+    // order.__set__("console", {
+    //   log: function(val) {
+    //     console.log(val);
+    //   }
+    // });
     order.__set__("inventoryData", this.testData);
   });
   it("order an item when there are enough in stock", function(done) {
+    var _this = this;
+
     order.orderItem("CCC", 3, function() {
+      //   expect(_this.console.log.callCount).to.equal(1);
       done();
     });
   });
